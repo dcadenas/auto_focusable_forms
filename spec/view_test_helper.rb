47d59260dc74762804b4d1530a67762c997cc34b
Daniel Cadenas <dcadenas@gmail.com>
2008-10-09 01:16:19 -0400

    minor changes to README and view_test_helper.rb

--- spec/view_test_helper.rb
+++ spec/view_test_helper.rb
@@ -21,20 +21,20 @@ module ViewTestHelper
     false
   end
 
-  def create_active_record_instance_variable class_name, attributes = {}
-    self.class.send :define_method, "#{class_name.underscore}_path" do "/#{class_name.underscore.pluralize}/1" end
-    self.class.send :define_method, "#{class_name.underscore.pluralize}_path" do "/#{class_name.underscore.pluralize}" end
-    self.class.send :define_method, "new_#{class_name.underscore}_path" do "/#{class_name.underscore.pluralize}/new" end
+  def create_active_record_instance_variable ar_class_name, attributes = {}
+    self.class.send :define_method, "#{ar_class_name.underscore}_path" do "/#{ar_class_name.underscore.pluralize}/1" end
+    self.class.send :define_method, "#{ar_class_name.underscore.pluralize}_path" do "/#{ar_class_name.underscore.pluralize}" end
+    self.class.send :define_method, "new_#{ar_class_name.underscore}_path" do "/#{ar_class_name.underscore.pluralize}/new" end
 
-    ar_class = create_class class_name
-    instance_variable = instance_variable_set("@#{class_name.underscore}", mock(class_name.underscore))
-    instance_variable.stub!(:class).and_return(ar_class)
-    instance_variable.stub!(:id).and_return(nil)
+    ar_class = create_class ar_class_name
+    ar_instance_variable = instance_variable_set("@#{ar_class_name.underscore}", mock(ar_class_name.underscore))
+    ar_instance_variable.stub!(:class).and_return(ar_class)
+    ar_instance_variable.stub!(:id).and_return(nil)
     attributes.each_pair do |field, value|
-      instance_variable.stub!(field).and_return(value)
+      ar_instance_variable.stub!(field).and_return(value)
     end
-    instance_variable.stub!(:new_record?).and_return(true)
-    instance_variable.stub!(:errors).and_return(mock('errors', :on => nil))
+    ar_instance_variable.stub!(:new_record?).and_return(true)
+    ar_instance_variable.stub!(:errors).and_return(mock('errors', :on => nil))
   end
 
   def create_class class_name
